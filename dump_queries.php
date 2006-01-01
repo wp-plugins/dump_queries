06039ccbe74942b6d137b5dde09327236ca18c9e
ryan <ryan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2006-01-01 23:52:55 +0000

    dump_queries
    git-svn-id: https://plugins.svn.wordpress.org/dump_queries/trunk@4348 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dump_queries.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: Dump Queries
+Plugin URI: http://dev.wp-plugins.org/browser/dump_queries/
+Description: Dump query and cache stats for debug purposes.
+Version: 1.0
+Author: Ryan Boren
+Author URI: http://boren.nu/
+*/
+
+/*
+The following must be added to wp-config.php for this plugin to be useful.
+
+define('SAVEQUERIES', true);
+
+*/
+
+function dq_dump_queries() {
+	global $wpdb;
+	$hide = true;
+
+	if ($hide) echo "<!--\n";
+	echo "<p>";
+	echo "<strong>Total Number of Queries:</strong> {$wpdb->num_queries}<br/>\n";
+	echo "<strong>Number of seconds:</strong> "; timer_stop(1); echo "<br/>\n\n";
+	echo "<strong>Memory Usage:</strong> " . memory_get_usage() . "<br/>\n\n";
+	echo "</p>";
+
+	foreach ($wpdb->queries as $query) {
+		echo "<p>";
+		echo "<strong>Query:</strong> {$query[0]}<br/>\n";
+		echo "<strong>Time:</strong> {$query[1]}\n";
+		echo "</p>";
+	}
+
+	global $wp_object_cache;
+	if ( ! isset($wp_object_cache) )
+		return;
+
+	$wp_object_cache->stats();
+	if ($hide) echo "\n-->\n";
+}
+
+add_action('wp_footer', 'dq_dump_queries');
+add_action('admin_footer', 'dq_dump_queries');
+?>
